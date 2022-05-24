320589d67aae87983739911b06bf0db13b669717
mederic mesle <mederic.mesle@gmail.com>
2022-05-24 17:14:13 +0200

    Fixed ChatroomChannel

--- app/javascript/controllers/chatrooms_subscription_controller.js
+++ app/javascript/controllers/chatrooms_subscription_controller.js
@@ -3,12 +3,9 @@ import consumer from "../channels/consumer"
 
 export default class extends Controller {
   static values = { chatroomId: Number }
+  static targets = ["messages"]
 
   connect() {
-    this.channel = consumer.subscriptions.create(
-      { channel: "ChatroomChannel", id: this.chatroomIdValue },
-      { received: data => this.messagesTarget.insertAdjacentHTML("beforeend", data) }
-    )
-    console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
+    console.log(`Subscribe to the chatroom with the id ${this.chatroomIdValue}.`)
   }
 }
